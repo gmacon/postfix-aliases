412d2c46989b1ce03a6b0b7c6f49a2ce26b574d2
George Macon <george.macon@gmail.com>
2016-12-18 15:20:23 -0500

    Add load_data command

--- postfix_aliases/app.py
+++ postfix_aliases/app.py
@@ -1,9 +1,10 @@
 import click
 from flask import Flask
 from flask_login import LoginManager
+import yaml
 
 from . import aliases, user
-from .models import db, Domain, Mailbox
+from .models import db, Alias, Domain, Mailbox
 from .password import hash_ssha512
 
 
@@ -45,5 +46,38 @@ def create_mailbox(password, email):
                       domain=domain_obj,
                       password=hash_ssha512(password))
     db.session.add(mailbox)
+
+    db.session.commit()
+
+
+@app.cli.command()
+@click.argument('alias_yaml', type=click.File('r'))
+def load_data(alias_yaml):
+    data = yaml.safe_load(alias_yaml)
+
+    class DomainLookup(dict):
+        def __missing__(self, key):
+            d = self[key] = Domain(name=key)
+            db.session.add(d)
+            return d
+
+    domains = DomainLookup()
+
+    for mailbox_addr, mailbox_info in data.items():
+        localpart, at, domainpart = mailbox_addr.partition('@')
+        password = mailbox_info['passwd']
+        aliases = mailbox_info.get('aliases', ())
+
+        mailbox = Mailbox(localpart=localpart,
+                          domain=domains[domainpart],
+                          password=password)
+        db.session.add(mailbox)
+
+        for alias in aliases:
+            localpart, at, domainpart = alias.partition('@')
+            alias = Alias(localpart=localpart,
+                          domain=domains[domainpart],
+                          mailbox=mailbox)
+            db.session.add(alias)
 
     db.session.commit()

f77680beae92689019c59cf6c5611f48fa3c42e2
George Macon <george.macon@gmail.com>
2016-12-18 15:19:48 -0500

    Load install_requres from requirements.in

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, find_packages
 
+
+with open('requirements.in', 'r') as req_file:
+      requirements = [l.strip() for l in req_file]
+
+
 setup(name='postfix_aliases',
       version='0.1',
       packages=find_packages(),
-      include_package_data=True)
+      include_package_data=True,
+      install_requires=requirements)

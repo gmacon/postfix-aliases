1266b197c6e319b0ae62ca3f59d59ee90394daea
George Macon <george.macon@gmail.com>
2016-12-18 17:04:40 -0500

    Add change password form

--- postfix_aliases/user/__init__.py
+++ postfix_aliases/user/__init__.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, redirect, render_template, url_for
-from flask_login import login_required, login_user, logout_user
+from flask import Blueprint, flash, redirect, render_template, url_for
+from flask_login import current_user, login_required, login_user, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, EqualTo
 
-from ..models import Domain, Mailbox
+from ..models import db, Mailbox
 from ..password import hash_ssha512, check_ssha512
 
 
@@ -33,3 +33,24 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('.login'))
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old Password', validators=[DataRequired()])
+    new_password = PasswordField('New Password',
+                                 validators=[DataRequired(),
+                                             EqualTo('confirm_password')])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired()])
+
+
+@bp.route('/changepassword', methods=['GET', 'POST'])
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if check_ssha512(form.old_password.data, current_user.password):
+            current_user.password = hash_ssha512(form.new_password.data)
+            db.session.commit()
+            flash('Password change successful')
+            return redirect('/')
+    return render_template('pwchange.html', form=form)

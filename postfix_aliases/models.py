f730b60c961d540f8a6d338cb259d193b30ed7e9
George Macon <george.macon@gmail.com>
2016-12-18 16:03:12 -0500

    Add __repr__ and __str__ to models

--- postfix_aliases/models.py
+++ postfix_aliases/models.py
@@ -9,6 +9,9 @@ class Domain(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True)
 
+    def __repr__(self):
+        return '<{}: {}>'.format(self.__class__.__name__, self.name)
+
 
 class Mailbox(UserMixin, db.Model):
     __table_args__ = (
@@ -20,6 +23,12 @@ class Mailbox(UserMixin, db.Model):
     domain_id = db.Column(db.Integer, db.ForeignKey('domain.id'))
     domain = db.relationship('Domain', backref='mailboxes')
     password = db.Column(db.String(128))
+
+    def __str__(self):
+        return '{}@{}'.format(self.localpart, self.domain.name)
+
+    def __repr__(self):
+        return '<{}: {}>'.format(self.__class__.__name__, self)
 
 
 class Alias(db.Model):
@@ -33,3 +42,10 @@ class Alias(db.Model):
     domain = db.relationship('Domain', backref='aliases')
     mailbox_id = db.Column(db.Integer, db.ForeignKey('mailbox.id'))
     mailbox = db.relationship('Mailbox', backref='aliases')
+
+    def __str__(self):
+        return '{}@{}'.format(self.localpart, self.domain.name)
+
+    def __repr__(self):
+        return '<{}: {} for {}>'.format(self.__class__.__name__, self,
+                                        self.mailbox)

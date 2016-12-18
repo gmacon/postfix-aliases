08831574411b6b4f925b51c108456877dc80ff2c
George Macon <george.macon@gmail.com>
2016-12-18 16:17:03 -0500

    Add some CRUD helpers to models

--- postfix_aliases/models.py
+++ postfix_aliases/models.py
@@ -3,6 +3,8 @@ from email.headerregistry import Address
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
+from .password import hash_ssha512
+
 
 db = SQLAlchemy()
 
@@ -10,6 +12,20 @@ db = SQLAlchemy()
 class Domain(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True)
+
+    @classmethod
+    def get(cls, name):
+        return cls.query.filter(cls.name == name.lower()).first()
+
+    @classmethod
+    def new(cls, name):
+        self = cls(name=name.lower())
+        db.session.add(self)
+        return self
+
+    @classmethod
+    def ensure(cls, name):
+        return cls.get(name) or cls.new(name)
 
     def __repr__(self):
         return '<{}: {}>'.format(self.__class__.__name__, self.name)
@@ -25,6 +41,33 @@ class Mailbox(UserMixin, db.Model):
     domain_id = db.Column(db.Integer, db.ForeignKey('domain.id'))
     domain = db.relationship('Domain', backref='mailboxes')
     password = db.Column(db.String(128))
+
+    @classmethod
+    def get(cls, addr):
+        addr = Address(addr_spec=addr)
+        return cls.query.join(Domain).filter(
+            cls.localpart == addr.username.lower(),
+            Domain.name == addr.domain.lower()).first()
+
+    @classmethod
+    def new(cls, addr, password=None):
+        addr = Address(addr_spec=addr)
+        domain = Domain.ensure(addr.domain)
+        self = cls(localpart=addr.username.lower(),
+                   domain=domain,
+                   password='')
+        if password is not None:
+            self.password = hash_ssha512(password)
+        db.session.add(self)
+        return self
+
+    def add_alias(self, addr):
+        addr = Address(addr_spec=addr)
+        alias = Alias(localpart=addr.username.lower(),
+                      domain=Domain.get(addr.domain),
+                      mailbox=self)
+        db.session.add(alias)
+        return alias
 
     def __str__(self):
         return str(Address(username=self.localpart,
